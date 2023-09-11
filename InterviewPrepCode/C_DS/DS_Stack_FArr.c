b6c2686dbc52ca90859dcd7ae23bc21749e96d6d
DanielisMellow <82124061+DanielisMellow@users.noreply.github.com>
2023-09-11 14:22:00 -0500

    F

--- InterviewPrepCode/C_DS/DS_Stack_FArr.c
+++ InterviewPrepCode/C_DS/DS_Stack_FArr.c
@@ -1,74 +1,56 @@
-#include <stdio.h>
 #include <stdint.h>
-
+#include <stdio.h>
 
 // Hello from sal in neo vim
 #define MAX_SIZE 10
 
-typedef struct
-{
-    int items[MAX_SIZE];
-    int top;
+typedef struct {
+  int items[MAX_SIZE];
+  int top;
 } Stack;
 
-void initialize(Stack *s)
-{
-    s->top = -1;
+void initialize(Stack *s) { s->top = -1; }
+
+int isEmpty(Stack *s) { return s->top == -1; }
+
+int isFull(Stack *s) { return s->top == MAX_SIZE - 1; }
+
+void push(Stack *s, int newItem) {
+  if (isFull(s))
+    printf("STACK FULL. PUSH FAILED\n");
+
+  else {
+    s->top += 1;
+    s->items[s->top] = newItem;
+  }
 }
 
-int isEmpty(Stack *s)
-{
-    return s->top == -1;
+int pop(Stack *s) {
+  if (isEmpty(s)) {
+    printf("STACK EMPTY. POP FAILED\n");
+    return -1;
+  }
+
+  int item = s->items[s->top];
+  s->top--;
+
+  return item;
 }
 
-int isFull(Stack *s)
-{
-    return s->top == MAX_SIZE - 1;
-}
+int main() {
 
-void push(Stack *s, int newItem)
-{
-    if (isFull(s))
-        printf("STACK FULL. PUSH FAILED\n");
+  Stack s;
+  initialize(&s);
 
-    else
-    {
-        s->top += 1;
-        s->items[s->top] = newItem;
-    }
-}
+  for (int i = 0; i < 16; i++) {
+    int item = i * 8;
+    printf("Attempting to Push: %d into stack\n", item);
+    push(&s, item);
+  }
 
-int pop(Stack *s)
-{
-    if (isEmpty(s))
-    {
-        printf("STACK EMPTY. POP FAILED\n");
-        return -1;
-    }
-
-    int item = s->items[s->top];
-    s->top--;
-
-    return item;
-}
-
-int main()
-{
-
-    Stack s;
-    initialize(&s);
-
-    for (int i = 0; i < 16; i++)
-    {
-        int item = i * 8;
-        printf("Attempting to Push: %d into stack\n", item);
-        push(&s, item);
-    }
-
-    for (int i = 0; i < 10; i++)
-    {
-        int item = pop(&s);
-        if (item != -1)
-            printf("%d was Popped from Stack\n", item);
-    }
+  for (int i = 0; i < 10; i++) {
+    int item = pop(&s);
+    if (item != -1)
+      printf("%d was Popped from Stack\n", item);
+  }
 }
