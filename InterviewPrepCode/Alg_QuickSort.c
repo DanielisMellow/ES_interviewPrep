7689d57cfd266e761f981f32b26bd729a3f65be6
DanielisMellow <82124061+DanielisMellow@users.noreply.github.com>
2023-09-25 12:12:35 -0500

    formatting

--- InterviewPrepCode/Alg_QuickSort.c
+++ InterviewPrepCode/Alg_QuickSort.c
@@ -1,70 +1,63 @@
 #include <stdio.h>
 
 // Function to swap two elements
-void swap(int *a, int *b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
 }
 
 // Partition the array using the last element as the pivot
-int partition(int arr[], int low, int high)
-{
-    // Choosing the pivot
-    int pivot = arr[high];
+int partition(int arr[], int low, int high) {
+  // Choosing the pivot
+  int pivot = arr[high];
 
-    int partitionIndex = low; // Set Partition Index as low initially
+  int partitionIndex = low; // Set Partition Index as low initially
 
-    for (int i = low; i < high; i++)
-    {
-        if (arr[i] < pivot)
-        {
-            // Swap current element if it is less than pivot
-            swap(&arr[partitionIndex], &arr[i]);
-            partitionIndex++; // Increment partition Index
-        }
+  for (int i = low; i < high; i++) {
+    if (arr[i] < pivot) {
+      // Swap current element if it is less than pivot
+      swap(&arr[partitionIndex], &arr[i]);
+      partitionIndex++; // Increment partition Index
     }
-    swap(&arr[partitionIndex], &arr[high]);
-    return partitionIndex;
+  }
+  swap(&arr[partitionIndex], &arr[high]);
+  return partitionIndex;
 }
 
 // The main function that implements QuickSort
 // arr[] --> Array to be sorted,
 // low --> Starting index,
 // high --> Ending index
-void quickSort(int arr[], int low, int high)
-{
-    if (low < high)
-    {
+void quickSort(int arr[], int low, int high) {
+  if (low < high) {
 
-        // pi is partitioning index, arr[p]
-        // is now at right place
-        int pi = partition(arr, low, high);
+    // pi is partitioning index, arr[p]
+    // is now at right place
+    int pi = partition(arr, low, high);
 
-        // Separately sort elements before
-        // partition and after partition
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
+    // Separately sort elements before
+    // partition and after partition
+    quickSort(arr, low, pi - 1);
+    quickSort(arr, pi + 1, high);
+  }
 }
 
-int main()
-{
+int main() {
 
-    int A[] = {7, 2, 1, 6, 8, 5, 3, 4};
-    // int A[] = {2, 0, 2, 1, 1, 0};
-    int size = sizeof(A) / sizeof(A[0]);
+  int A[] = {7, 2, 1, 6, 8, 5, 3, 4};
+  // int A[] = {2, 0, 2, 1, 1, 0};
+  int size = sizeof(A) / sizeof(A[0]);
 
-    for (int i = 0; i < size; i++)
-        printf("%d ", A[i]);
+  for (int i = 0; i < size; i++)
+    printf("%d ", A[i]);
 
-    printf("\n");
-    quickSort(A, 0, size - 1);
+  printf("\n");
+  quickSort(A, 0, size - 1);
 
-    for (int i = 0; i < size; i++)
-        printf("%d ", A[i]);
+  for (int i = 0; i < size; i++)
+    printf("%d ", A[i]);
 
-    printf("\n");
-    return 0;
+  printf("\n");
+  return 0;
 }
