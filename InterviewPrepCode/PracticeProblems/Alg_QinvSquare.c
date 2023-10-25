71d03b31a46e2684c3cef483b7018f4e790c1af9
DanielisMellow <82124061+DanielisMellow@users.noreply.github.com>
2023-10-25 13:05:17 -0500

    working progress

---
+++ InterviewPrepCode/PracticeProblems/Alg_QinvSquare.c
@@ -0,0 +1,36 @@
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+float Q_rsqrt(float number) {
+  int32_t i;
+  float x2, y;
+  const float threehalfs = 1.5F;
+  x2 = number * 0.5;
+
+  y = number;
+  i = *(int32_t *)&y;        // Evil Floating point bit level hacking
+  i = 0x5f3759df - (i >> 1); // what the fuck
+  y = *(float *)&i;
+  y = y * (threehalfs - (x2 * y * y)); // first iteration
+  // y = y *(threehalfs - (x2 *y *y)); //Second iteration can be remove can be
+  // removed
+
+  return y;
+}
+
+float rsqrt(float number) { return 1 / sqrt(number); }
+
+int main(int argc, char *argv[]) {
+
+  for (int i = 1; i < 10; i++) {
+
+    printf("Q_rsqrt: %f", Q_rsqrt(i));
+    printf("\trsqrt: %f\n", rsqrt(i));
+  }
+  printf("size of int: %lu\n", sizeof(int));
+  printf("size of long: %lu\n", sizeof(long));
+  printf("size of float: %lu\n", sizeof(float));
+  return EXIT_SUCCESS;
+}
