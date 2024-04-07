0f3cfe6cd36b349fa927aa330ede6c744a0af16e
DanielisMellow <82124061+DanielisMellow@users.noreply.github.com>
2024-04-07 17:04:51 -0500

    Adding comments to kmt alg

--- InterviewPrepCode/EmbeddedPrep/Array/knuth_morris_pratt.c
+++ InterviewPrepCode/EmbeddedPrep/Array/knuth_morris_pratt.c
@@ -2,53 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 
-void compute_lps_array(char *pat, int M, int *lps) {
-  int len = 0;
-  lps[0] = 0;
+void compute_lps_array(char *pat, int pattern_len, int *lps) {
+  int prefix_len = 0;
+  lps[0] = 0; // longest prefix suffix
   int i = 1;
 
-  while (i < M) {
-    if (pat[i] == pat[len]) {
-      len++;
-      lps[i] = len;
+  while (i < pattern_len) {
+    if (pat[i] == pat[prefix_len]) {
+      prefix_len++;
+      lps[i] = prefix_len;
       i++;
-    } else { // pat[i] != pat[len]
-      if (len != 0) {
-        len = lps[len - 1];
-      } else {
-        lps[i] = 0;
-        i++;
-      }
+    } else if (prefix_len != 0) {
+      prefix_len = lps[prefix_len - 1];
+    } else {
+      // 'prefix_len' reached 0 so save that into lps[] move forward
+      lps[i] = 0;
+      i++;
     }
   }
 }
 
 void kmp_search(char *pat, char *txt) {
-  int M = strlen(pat);
-  int N = strlen(txt);
+  int pattern_len = strlen(pat);
+  int text_len = strlen(txt);
 
-  int *lps = (int *)malloc(sizeof(int) * M);
+  int *lps = (int *)malloc(sizeof(int) * pattern_len);
+  compute_lps_array(pat, pattern_len, lps);
 
-  int j = 0;
-
-  compute_lps_array(pat, M, lps);
-
-  for (int i = 0; i < M; i++) {
+  for (int i = 0; i < pattern_len; i++) {
     printf("%d", lps[i]);
   }
   printf("\n");
-  int i = 0;
-  while (i < N) {
+
+  int i = 0; // the position of the current character in the text
+  int j = 0; // the postionn of the current character in the pattern
+  while (i < text_len) {
     if (pat[j] == txt[i]) {
       j++;
       i++;
     }
-    if (j == M) {
+    if (j == pattern_len) {
       printf("Found pattern at index %d \n", i - j);
-      j = lps[j - 1];
+      j = lps[j - 1]; // reset
     }
     // mismatch after j matches
-    else if (i < N && pat[j] != txt[i]) {
+    else if (i < text_len && pat[j] != txt[i]) {
       // Do not match lps[0..lps[j-1]] characters,
       // they will match anyway
       if (j != 0)
@@ -63,8 +61,8 @@ void kmp_search(char *pat, char *txt) {
 
 int main(int argc, char *argv[]) {
 
-  char *txt = "ABABDABACDABABCABAB";
-  char *pat = "ABABCABAB";
+  char *txt = "----ABCXXXABAB-----ABCXXXABCB";
+  char *pat = "ABCXXXABCB";
   kmp_search(pat, txt);
   return 0;
   return EXIT_SUCCESS;
