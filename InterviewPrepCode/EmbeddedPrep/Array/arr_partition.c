751f41e12a692789b7d86f7a6f5eb774c23aa177
DanielisMellow <82124061+DanielisMellow@users.noreply.github.com>
2024-04-19 10:56:42 -0500

    Printing arr

--- InterviewPrepCode/EmbeddedPrep/Array/arr_partition.c
+++ InterviewPrepCode/EmbeddedPrep/Array/arr_partition.c
@@ -12,11 +12,19 @@ int sum_of_partions(int a[], int size) {
   }
   return sum;
 }
+void print_arr(int a[], int size) {
+
+  for (int i = 0; i < size; i++) {
+    printf("%d ", a[i]);
+  }
+  printf("\n");
+}
 
 int main(int argc, char *argv[]) {
   int arr[] = {1, 4, 3, 2};
   int size = sizeof(arr) / sizeof(arr[0]);
+  print_arr(arr, size);
   int sum = sum_of_partions(arr, size);
-  printf("%d\n", sum);
+  printf("Sum of partition: %d\n", sum);
   return EXIT_SUCCESS;
 }
